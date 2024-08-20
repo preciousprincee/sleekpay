1b383f74438f2f2246cfb950d951540fa26bb4c9
preciousprincee <pchukwuebuka964@gmail.com>
2024-08-20 12:44:28 +0100

    first commit

--- sleekpay/settings.py
+++ sleekpay/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = 'django-insecure-^6n3k!k(vj%r#7=ob5nan=x81sj#z!41u1yq4zyuv#kd88(^^w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
